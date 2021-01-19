7e89c79a32642c110a49d00423385574b915099e
Shaina C <scailles23@gmail.com>
2021-01-19 09:14:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,4 +6,4 @@ html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
-doc.css(".headline-260IBN").text 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
